bf43843b63f52b773fa9fa66c6d835366eebbe54
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2023-06-19 10:11:17 +0200

    Refactor building standalone-template to avoid using undeclared variables.

--- scripts/build-templates/standalone-template.amd.js
+++ scripts/build-templates/standalone-template.amd.js
@@ -1,10 +1,9 @@
-/** 
+/**
  * Include this template file after backbone-forms.amd.js to override the default templates
  *
  * 'data-*' attributes control where elements are placed
  */
-define(['jquery', 'underscore', 'backbone', 'backbone-forms'], function($, _, Backbone) {
-  var Form = Backbone.Form;
+define(['jquery', 'underscore', 'backbone', 'backbone-forms'], function($, _, Backbone, Form) {
 
   {{body}}
 
