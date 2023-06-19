b5d062660b1cc691e6ebad143822d75b71788d54
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2023-06-19 08:55:19 +0200

    Refactor and cleanup AMD module script and List editor code
    The changes in this commit refactors and cleans up the AMD module script and List editor code, by redefining Form to be explicitly required as a dependency in the AMD module script, and removing and consolidating some code in the List editor implementation.

--- scripts/build-templates/simple.amd.js
+++ scripts/build-templates/simple.amd.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore', 'backbone', 'backbone-forms'], function($, _, Backbone) {
+define(['jquery', 'underscore', 'backbone', 'backbone-forms'], function($, _, Backbone, Form) {
 
   {{body}}
 
